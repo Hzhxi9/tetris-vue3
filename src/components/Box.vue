4eb410db1469f3c9e1b50feeebc4bf08051e364a
huangzx <huangzhixi@youxin.cloud>
2021-09-25 20:02:42 +0800

    game

---
+++ src/components/Box.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { computed } from "@vue/reactivity";
+
+const props = defineProps({
+  type: {
+    default: 0,
+    type: Number,
+  },
+});
+const typeToColorMap: any = {
+  [-1]: "red",
+  [0]: "gray",
+  [1]: "red",
+};
+const color = computed(() => typeToColorMap[props.type]);
+</script>
+
+<template>
+  <div class="box">{{ type }}</div>
+</template>
+
+<style scoped>
+.box {
+  width: 20px;
+  height: 20px;
+  background: v-bind(color);
+  margin: 2px;
+}
+</style>
---
+++ src/components/Game.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Box from './Box.vue'
+import { reactive } from '@vue/reactivity';
+
+const map = reactive([])
+</script>
+<template>
+    <div>
+        <div class="row" v-for="row in 10" :key="row">
+            <div v-for="col in 10" :key="col">
+                <Box :type="map[row - 1][col - 1]"></Box>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.row {
+    display: flex;
+}
+.game {
+    margin: 0 15px;
+}
+</style>
